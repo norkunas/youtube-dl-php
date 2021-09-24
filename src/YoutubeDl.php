dce29787841b6da886ed76cffe8863ac502aa941
Tomas <norkunas.tom@gmail.com>
2021-09-24 13:14:23 +0300

    Allow nullable processTarget in progress callback

--- src/YoutubeDl.php
+++ src/YoutubeDl.php
@@ -57,7 +57,7 @@ class YoutubeDl
         $this->processBuilder = $processBuilder ?? new DefaultProcessBuilder();
         $this->metadataReader = $metadataReader ?? new DefaultMetadataReader();
         $this->filesystem = $filesystem ?? new Filesystem();
-        $this->progress = static function (string $progressTarget, string $percentage, string $size, ?string $speed, ?string $eta, ?string $totalTime): void {};
+        $this->progress = static function (?string $progressTarget, string $percentage, string $size, ?string $speed, ?string $eta, ?string $totalTime): void {};
         $this->debug = static function (string $type, string $buffer): void {};
     }
 
