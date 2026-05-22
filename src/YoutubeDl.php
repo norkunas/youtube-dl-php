2bc6b6eb97340a07460eeddbf1ad456194a0c7f7
M Asef <46367425+asefsoft@users.noreply.github.com>
2026-05-22 17:39:55 +0330

    Refactor thumbnail info line check with regex

--- src/YoutubeDl.php
+++ src/YoutubeDl.php
@@ -217,7 +217,7 @@ class YoutubeDl
                 continue;
             }
 
-            if (str_starts_with($line, '[info] Available thumbnails for')) {
+            if (preg_match('/^\[info\] (?:Available thumbnails|Thumbnails) for/', $line)) {
                 $parsing = 'table_header';
             } elseif ($parsing === 'table_header') {
                 $header = $line;
